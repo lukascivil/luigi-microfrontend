a266b3a01dba402f54848d2705887cd378a11145
lukascivil <lukas_civil@hotmail.com>
2024-02-17 14:24:51 -0300

    chore: migrate to yarn workspace

---
+++ apps/remote/src/main.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./views/home";
+import Sample1 from "./views/sample1";
+import Sample2 from "./views/sample2";
+import { addInitListener } from "@luigi-project/client";
+import "./index.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    addInitListener(() => {
+      console.log("Luigi Client initialized.");
+    });
+  }
+
+  render() {
+    return (
+      <BrowserRouter basename={`sampleapp.html#`}>
+        <Route path="/home" component={Home} />
+        <Route path="/sample1" component={Sample1} />
+        <Route path="/sample2" component={Sample2} />
+      </BrowserRouter>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
---
+++ apps/remote/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  root: "remote/src",
+  server: {
+    port: 3002,
+  },
+  plugins: [
+    react({
+      jsxRuntime: "classic",
+    }),
+    viteTsconfigPaths(),
+  ],
+  build: {
+    outDir: "../dist",
+  },
+});
